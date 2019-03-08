e451dd051a1d5f89a3052f4f4a0ad6878895dfc9
s777610 <s777610@gmail.com>
2019-03-07 18:55:45 -0800

    implement clients component

---
+++ src/components/clients/Clients.js
@@ -0,0 +1,77 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+
+class Clients extends Component {
+  render() {
+    const clients = [
+      {
+        id: "34234329",
+        firstName: "Wilson",
+        lastName: "Hung",
+        email: "s777610@gmail.com",
+        phone: "6193661344",
+        balance: "30.45"
+      },
+      {
+        id: "34234329",
+        firstName: "Wilson",
+        lastName: "Hung",
+        email: "s777610@gmail.com",
+        phone: "6193661344",
+        balance: "30"
+      }
+    ];
+
+    if (clients) {
+      return (
+        <div>
+          <div className="row">
+            <div className="col-md-6">
+              <h2>
+                <i className="fas fa-users" />
+                Clients
+              </h2>
+            </div>
+            <div className="col-md-6" />
+          </div>
+
+          <table className="table table-striped">
+            <thead className="thead-inverse">
+              <tr>
+                <th>Name</th>
+                <th>Email</th>
+                <th>Balance</th>
+                <th />
+              </tr>
+            </thead>
+            <tbody>
+              {clients.map(client => {
+                return (
+                  <tr key={client.id}>
+                    <td>
+                      {client.firstName} {client.lastName}
+                    </td>
+                    <td>{client.email}</td>
+                    <td> $ {parseFloat(client.balance).toFixed(2)}</td>
+                    <td>
+                      <Link
+                        to={`/client/${client.id}`}
+                        className="btn btn-secondary btn-sm"
+                      >
+                        <i className="fas fa-arrow-circle-right" /> Details
+                      </Link>
+                    </td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+        </div>
+      );
+    } else {
+      return <h1>Loading ... </h1>;
+    }
+  }
+}
+
+export default Clients;
