fd2d878d42c37a9e9c7721b054ef769307b9860e
s777610 <s777610@gmail.com>
2019-03-08 14:27:48 -0800

    sum balance of all clients

--- src/components/clients/Clients.js
+++ src/components/clients/Clients.js
@@ -1,25 +1,39 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
 import { Link } from "react-router-dom";
 import { compose } from "redux";
 import { connect } from "react-redux";
 import { firestoreConnect } from "react-redux-firebase";
-import PropTypes from "prop-types";
+
+import Spinner from "../layout/Spinner";
 
 class Clients extends Component {
   render() {
     const { clients } = this.props;
+    let totalBalance;
 
     if (clients) {
+      totalBalance = clients.reduce(
+        (sum, client) => sum + parseFloat(client.balance),
+        0
+      );
+
       return (
-        <div>
-          <div className="row">
-            <div className="col-md-6">
-              <h2>
-                <i className="fas fa-users" />
-                Clients
-              </h2>
+        <Fragment>
+          <div>
+            <div className="row">
+              <div className="col-md-6">
+                <h2>
+                  {" "}
+                  <i className="fas fa-users" /> Clients{" "}
+                </h2>
+              </div>
+              <div className="col-md-6">
+                <h5 className="text-right text-secondary">
+                  Total Owed{" "}
+                  <span className="text-primary">${totalBalance}</span>
+                </h5>
+              </div>
             </div>
-            <div className="col-md-6" />
           </div>
 
           <table className="table table-striped">
@@ -32,44 +46,33 @@ class Clients extends Component {
               </tr>
             </thead>
             <tbody>
-              {clients.map(client => {
-                return (
-                  <tr key={client.id}>
-                    <td>
-                      {client.firstName} {client.lastName}
-                    </td>
-                    <td>{client.email}</td>
-                    <td> $ {parseFloat(client.balance).toFixed(2)}</td>
-                    <td>
-                      <Link
-                        to={`/client/${client.id}`}
-                        className="btn btn-secondary btn-sm"
-                      >
-                        <i className="fas fa-arrow-circle-right" /> Details
-                      </Link>
-                    </td>
-                  </tr>
-                );
-              })}
+              {clients.map(client => (
+                <tr key={client.id}>
+                  <td>
+                    {client.firstName} {client.lastName}
+                  </td>
+                  <td>{client.email}</td>
+
+                  <td>${parseFloat(client.balance).toFixed(2)}</td>
+                  <td>
+                    <Link
+                      to={`/client/${client.id}`}
+                      className="btn btn-secondary btn-sm"
+                    >
+                      <i className="fas fa-arrow-circle-right" /> Details
+                    </Link>
+                  </td>
+                </tr>
+              ))}
             </tbody>
           </table>
-        </div>
+        </Fragment>
       );
-    } else {
-      return <h1>Loading ... </h1>;
-    }
+    } else return <Spinner />;
   }
 }
 
-Clients.propTypes = {
-  firestore: PropTypes.object.isRequired,
-  clients: PropTypes.array
-};
-
 export default compose(
   firestoreConnect([{ collection: "clients" }]),
-  connect((state, props) => ({
-    // we can access this.props.client
-    clients: state.firestore.ordered.clients
-  }))
+  connect((state, props) => ({ clients: state.firestore.ordered.clients }))
 )(Clients);
