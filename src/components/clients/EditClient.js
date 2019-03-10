0fb04f25cfd6b9874b0c470320b2caa10a9fd4a9
s777610 <s777610@gmail.com>
2019-03-09 20:34:39 -0800

    add EditClient component, and handle two case of updating client. first, using componentDidUpdate in case of reloading page. Second, put this.props.client directly into state for not reloading page

---
+++ src/components/clients/EditClient.js
@@ -0,0 +1,153 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import { compose } from "redux";
+import { connect } from "react-redux";
+import { firestoreConnect } from "react-redux-firebase";
+import Spinner from "../layout/Spinner";
+import PropTypes from "prop-types";
+
+class EditClient extends Component {
+  state = {
+    firstName: "",
+    lastName: "",
+    email: "",
+    phone: "",
+    balance: "",
+    ...this.props.client
+  };
+
+  // for in case of reloading page
+  componentDidUpdate() {
+    // if state is empty
+    if (Object.keys(this.state).every(key => this.state[key].length === 0)) {
+      this.setState({ ...this.props.client });
+    }
+  }
+
+  onChange = e => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+
+  onSubmit = e => {
+    e.preventDefault();
+
+    const { client, firestore, history } = this.props;
+
+    // Updated Client
+    const updatedClient = { ...this.state };
+
+    // Update client in firestore
+    firestore
+      .update({ collection: "clients", doc: client.id }, updatedClient)
+      .then(() => history.push("/"));
+  };
+
+  render() {
+    const { client } = this.props;
+    const { firstName, lastName, phone, email, balance } = this.state;
+
+    if (client) {
+      return (
+        <div>
+          <div className="row">
+            <div className="col-md-6">
+              <Link to="/" className="btn btn-link">
+                <i className="fas fa-arrow-circle-left" /> Back To Dashboard
+              </Link>
+            </div>
+          </div>
+
+          <div className="card">
+            <div className="card-header">Edit Client</div>
+            <div className="card-body">
+              <form onSubmit={this.onSubmit}>
+                <div className="form-group">
+                  <label htmlFor="firstName">First Name</label>
+                  <input
+                    type="text"
+                    className="form-control"
+                    name="firstName"
+                    minLength="2"
+                    required
+                    onChange={this.onChange}
+                    value={firstName}
+                  />
+                </div>
+
+                <div className="form-group">
+                  <label htmlFor="lastName">Last Name</label>
+                  <input
+                    type="text"
+                    className="form-control"
+                    name="lastName"
+                    minLength="2"
+                    required
+                    onChange={this.onChange}
+                    value={lastName}
+                  />
+                </div>
+
+                <div className="form-group">
+                  <label htmlFor="email">Email</label>
+                  <input
+                    type="email"
+                    className="form-control"
+                    name="email"
+                    onChange={this.onChange}
+                    value={email}
+                  />
+                </div>
+
+                <div className="form-group">
+                  <label htmlFor="phone">Phone</label>
+                  <input
+                    type="text"
+                    className="form-control"
+                    name="phone"
+                    minLength="10"
+                    required
+                    onChange={this.onChange}
+                    value={phone}
+                  />
+                </div>
+
+                <div className="form-group">
+                  <label htmlFor="balance">Balance</label>
+                  <input
+                    type="text"
+                    className="form-control"
+                    name="balance"
+                    onChange={this.onChange}
+                    value={balance}
+                  />
+                </div>
+
+                <input
+                  type="submit"
+                  value="Submit"
+                  className="btn btn-primary btn-block"
+                />
+              </form>
+            </div>
+          </div>
+        </div>
+      );
+    } else {
+      return <Spinner />;
+    }
+  }
+}
+
+EditClient.propTypes = {
+  firestore: PropTypes.object.isRequired
+};
+
+export default compose(
+  firestoreConnect(props => [
+    { collection: "clients", storeAs: "client", doc: props.match.params.id }
+  ]),
+  // { firestore: { ordered } } is state
+  connect(({ firestore: { ordered } }, props) => ({
+    client: ordered.client && ordered.client[0] // if client exists get first element
+  }))
+)(EditClient);
