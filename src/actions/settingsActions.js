510792922a4b1c060a6ac70cc2e88a3c99153bb5
s777610 <s777610@gmail.com>
2019-03-12 12:01:30 -0700

    able to save settings into localstorage

--- src/actions/settingsActions.js
+++ src/actions/settingsActions.js
@@ -5,19 +5,49 @@ import {
 } from "./types";
 
 export const setDisableBalanceOnAdd = () => {
+  // Get settings from localStore
+  const settings = JSON.parse(localStorage.getItem("settings"));
+
+  // Toggle
+  settings.disableBalanceOnAdd = !settings.disableBalanceOnAdd;
+
+  // Setback to localStorage
+  localStorage.setItem("settings", JSON.stringify(settings));
+
   return {
-    type: DISABLE_BALANCE_ON_ADD
+    type: DISABLE_BALANCE_ON_ADD,
+    payload: settings.disableBalanceOnAdd
   };
 };
 
 export const setDisableBalanceOnEdit = () => {
+  // Get settings from localStore
+  const settings = JSON.parse(localStorage.getItem("settings"));
+
+  // Toggle
+  settings.disableBalanceOnEdit = !settings.disableBalanceOnEdit;
+
+  // Setback to localStorage
+  localStorage.setItem("settings", JSON.stringify(settings));
+
   return {
-    type: DISABLE_BALANCE_ON_EDIT
+    type: DISABLE_BALANCE_ON_EDIT,
+    payload: settings.disableBalanceOnEdit
   };
 };
 
 export const setAllowRegistration = () => {
+  // Get settings from localStore
+  const settings = JSON.parse(localStorage.getItem("settings"));
+
+  // Toggle
+  settings.allowRegistration = !settings.allowRegistration;
+
+  // Setback to localStorage
+  localStorage.setItem("settings", JSON.stringify(settings));
+
   return {
-    type: ALLOW_REGISTRAION
+    type: ALLOW_REGISTRAION,
+    payload: settings.allowRegistration
   };
 };
